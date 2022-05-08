91db7bf36da0cccb5c214752e7e832cf4ddd5bb8
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-05-08 20:35:21 +0200

    d

--- C01/ex03/srcs/main.cpp
+++ C01/ex03/srcs/main.cpp
@@ -6,7 +6,29 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:10:49 by vchevill          #+#    #+#             */
-/*   Updated: 2022/05/06 17:16:58 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/05/06 17:23:26 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Weapon.hpp"
+#include "../includes/HumanA.hpp"
+#include "../includes/HumanB.hpp"
+#include <iostream>
+int main()
+{
+	{
+		Weapon        club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon        club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+}
